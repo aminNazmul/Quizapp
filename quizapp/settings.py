1ec555b0e86baac9dfda23f1cbbf92f76e3cc5b5
aminNazmul <amin35-1029@diu.edu.bd>
2025-12-29 04:00:09 +0600

    Update settings.py

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -33,6 +33,11 @@ CSRF_TRUSTED_ORIGINS = [
     "https://quizapp-srmj.onrender.com",
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+
 
 # Application definition
 
